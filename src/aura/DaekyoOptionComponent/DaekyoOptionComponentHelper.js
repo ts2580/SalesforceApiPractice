d4518dba135d9f41025b1469082951bf2b5247a9
한성진 <trstyq@gmail.com>
2022-11-22 17:06:48 +0900

    Heroku Connect로 External Object 붙이기 시작

--- src/aura/DaekyoOptionComponent/DaekyoOptionComponentHelper.js
+++ src/aura/DaekyoOptionComponent/DaekyoOptionComponentHelper.js
@@ -5,24 +5,68 @@
 ({
     getOption : function(component, event, helper){
 
-        var action = component.get("c.GetOption");
+        let action = component.get("c.GetOption");
 
         action.setParams({
             "recordId" : component.get("v.recordId")
         })
 
         action.setCallback(this, function(response){
-            var state = response.getState();
+            let state = response.getState();
             if(state === "SUCCESS" && component.isValid()){
 
-                var ReturnedOption =  response.getReturnValue();
+                let MapOptionOptionDetail =  response.getReturnValue();
 
-                component.set("v.option", ReturnedOption);
+                let i = 1;
+                for (let key in MapOptionOptionDetail) {
 
-
-                let aaa = component.get("v.option");
-                console.dir(aaa);
-
+                    $A.createComponent(
+                        "lightning:input",
+                        {
+                            "label": "옵션 " + i,
+                            "value": key,
+                            // "onclick": component.getReference("c.addOption")
+                        },
+                        function(option, status, errorMessage){
+                            if (status === "SUCCESS") {
+                                let body = component.get("v.body");
+                                body.push(option);
+                                component.set("v.body", body);
+                                let j = 1;
+                                for (let detailedOption of MapOptionOptionDetail[key]){
+                                    $A.createComponent(
+                                        "lightning:input",
+                                        {
+                                            "label": "상세옵션 " + j,
+                                            "value": detailedOption["Name"],
+                                            "aura:id": detailedOption["Id"],
+                                        },
+                                        function(option, status, errorMessage){
+                                            if (status === "SUCCESS") {
+                                                let body = component.get("v.body");
+                                                body.push(option);
+                                                component.set("v.body", body);
+                                            }else if (status === "INCOMPLETE") {
+                                                console.log("서버 응답 없음")
+                                            }
+                                            else if (status === "ERROR") {
+                                                console.log("에러: " + errorMessage);
+                                            }
+                                        }
+                                    );
+                                    j++;
+                                }
+                            }
+                            else if (status === "INCOMPLETE") {
+                                console.log("서버 응답 없음")
+                            }
+                            else if (status === "ERROR") {
+                                console.log("에러: " + errorMessage);
+                            }
+                        }
+                    );
+                    i++;
+                }
             }
         });
 
